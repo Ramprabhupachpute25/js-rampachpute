6220bdd773e1a50a1b025e649a647a5e4cc661ca
Ramprabhupachpute25 <ramprabhupachpute302@gmail.com>
2025-03-23 11:39:50 +0000

    for loop understanding

---
+++ 01-basics/one.js
@@ -0,0 +1,22 @@
+ let myArray =["tata","mahindra","kia","hyudai"]
+//  console.log(myArray.length);
+ 
+for (let index = 0; index <myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(`${element}`);
+    
+    
+ }
+
+
+ for (let index = 0; index <=20; index++) {
+    if (index==5) {
+
+        console.log(`5 detected`);
+        continue
+        
+    }
+        console.log(`value of i is:${index}`);
+    
+    
+ }
